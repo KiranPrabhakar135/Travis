92c1d5f1f1f86c86f94f91a32f8cb9308a33a5fd
kiranprabhakar135 <kiranprabhakar135@gmail.com>
2019-10-23 20:48:06 -0400

    updating the yaml1

--- src/main/java/com/travisci/travisci/home/HomeController.java
+++ src/main/java/com/travisci/travisci/home/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/home")
     public String sayHi(){
-        return "Welcome to Travis CI";
+        return "Demo of Travis CI Deployment";
     }
 }
